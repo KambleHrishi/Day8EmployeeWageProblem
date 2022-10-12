85b017aae6f809a9bc3d73459b3d7c159150487d
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-12 21:32:15 +0530

    Employee Attendance check

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         public static void Main(string[]args)
         {
-            Console.WriteLine("Welcome To Employee Wage Computation Problem");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();// new object creation
+            int empCheck = random.Next(0, 2);//0 is inclusive,2 is exclusive: 0,1
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("employee is Absent");
+            }
         }
     }
 }
